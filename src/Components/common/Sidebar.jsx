e691137c590248bd0ad736e8dec26cf5400f3f14
bappy3a <sunnobappy@gmail.com>
2022-07-18 00:49:18 +0600

    cls name chage

--- src/Components/common/Sidebar.jsx
+++ src/Components/common/Sidebar.jsx
@@ -3,81 +3,81 @@ import React, { Component } from 'react'
 export default class Sidebar extends Component {
   render() {
     return (
-        <nav class="sidebar sidebar-offcanvas" id="sidebar">
-          <ul class="nav">
-            <li class="nav-item nav-category">Main Menu</li>
-            <li class="nav-item">
-              <a class="nav-link" href="index.html">
-                <i class="menu-icon typcn typcn-document-text"></i>
-                <span class="menu-title">Dashboard</span>
+        <nav className="sidebar sidebar-offcanvas" id="sidebar">
+          <ul className="nav">
+            <li className="nav-item nav-category">Main Menu</li>
+            <li className="nav-item">
+              <a className="nav-link" href="index.html">
+                <i className="menu-icon typcn typcn-document-text"></i>
+                <span className="menu-title">Dashboard</span>
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-                <i class="menu-icon typcn typcn-coffee"></i>
-                <span class="menu-title">Basic UI Elements</span>
-                <i class="menu-arrow"></i>
+            <li className="nav-item">
+              <a className="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+                <i className="menu-icon typcn typcn-coffee"></i>
+                <span className="menu-title">Basic UI Elements</span>
+                <i className="menu-arrow"></i>
               </a>
-              <div class="collapse" id="ui-basic">
-                <ul class="nav flex-column sub-menu">
-                  <li class="nav-item">
-                    <a class="nav-link" href="pages/ui-features/buttons.html">Buttons</a>
+              <div className="collapse" id="ui-basic">
+                <ul className="nav flex-column sub-menu">
+                  <li className="nav-item">
+                    <a className="nav-link" href="pages/ui-features/buttons.html">Buttons</a>
                   </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="pages/ui-features/dropdowns.html">Dropdowns</a>
+                  <li className="nav-item">
+                    <a className="nav-link" href="pages/ui-features/dropdowns.html">Dropdowns</a>
                   </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="pages/ui-features/typography.html">Typography</a>
+                  <li className="nav-item">
+                    <a className="nav-link" href="pages/ui-features/typography.html">Typography</a>
                   </li>
                 </ul>
               </div>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="pages/forms/basic_elements.html">
-                <i class="menu-icon typcn typcn-shopping-bag"></i>
-                <span class="menu-title">Form elements</span>
+            <li className="nav-item">
+              <a className="nav-link" href="pages/forms/basic_elements.html">
+                <i className="menu-icon typcn typcn-shopping-bag"></i>
+                <span className="menu-title">Form elements</span>
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="pages/charts/chartjs.html">
-                <i class="menu-icon typcn typcn-th-large-outline"></i>
-                <span class="menu-title">Charts</span>
+            <li className="nav-item">
+              <a className="nav-link" href="pages/charts/chartjs.html">
+                <i className="menu-icon typcn typcn-th-large-outline"></i>
+                <span className="menu-title">Charts</span>
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="pages/tables/basic-table.html">
-                <i class="menu-icon typcn typcn-bell"></i>
-                <span class="menu-title">Tables</span>
+            <li className="nav-item">
+              <a className="nav-link" href="pages/tables/basic-table.html">
+                <i className="menu-icon typcn typcn-bell"></i>
+                <span className="menu-title">Tables</span>
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="pages/icons/font-awesome.html">
-                <i class="menu-icon typcn typcn-user-outline"></i>
-                <span class="menu-title">Icons</span>
+            <li className="nav-item">
+              <a className="nav-link" href="pages/icons/font-awesome.html">
+                <i className="menu-icon typcn typcn-user-outline"></i>
+                <span className="menu-title">Icons</span>
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
-                <i class="menu-icon typcn typcn-document-add"></i>
-                <span class="menu-title">User Pages</span>
-                <i class="menu-arrow"></i>
+            <li className="nav-item">
+              <a className="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
+                <i className="menu-icon typcn typcn-document-add"></i>
+                <span className="menu-title">User Pages</span>
+                <i className="menu-arrow"></i>
               </a>
-              <div class="collapse" id="auth">
-                <ul class="nav flex-column sub-menu">
-                  <li class="nav-item">
-                    <a class="nav-link" href="pages/samples/blank-page.html"> Blank Page </a>
+              <div className="collapse" id="auth">
+                <ul className="nav flex-column sub-menu">
+                  <li className="nav-item">
+                    <a className="nav-link" href="pages/samples/blank-page.html"> Blank Page </a>
                   </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="pages/samples/login.html"> Login </a>
+                  <li className="nav-item">
+                    <a className="nav-link" href="pages/samples/login.html"> Login </a>
                   </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="pages/samples/register.html"> Register </a>
+                  <li className="nav-item">
+                    <a className="nav-link" href="pages/samples/register.html"> Register </a>
                   </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="pages/samples/error-404.html"> 404 </a>
+                  <li className="nav-item">
+                    <a className="nav-link" href="pages/samples/error-404.html"> 404 </a>
                   </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="pages/samples/error-500.html"> 500 </a>
+                  <li className="nav-item">
+                    <a className="nav-link" href="pages/samples/error-500.html"> 500 </a>
                   </li>
                 </ul>
               </div>
